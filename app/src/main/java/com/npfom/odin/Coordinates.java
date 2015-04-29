2a43d3c32a965caec4734b525e598e131090045d
Futilitymonster <kasper.thim@gmail.com>
2015-04-29 09:16:19 +0200

    Utility classes for connection handling with database

--- app/src/main/java/com/npfom/odin/Coordinates.java
+++ app/src/main/java/com/npfom/odin/Coordinates.java
@@ -2,23 +2,25 @@ package com.npfom.odin;
 
 /**
  * Created by Kasper on 29-Apr-15.
+ *
+ * Simple wrapper class for latitude-longitude coordinates
  */
 public class Coordinates {
 
-    private double xcoord;
-    private double ycoord;
+    private double lat;
+    private double lng;
 
-    public Coordinates(double x, double y){
-        xcoord = x;
-        ycoord = y;
+    public Coordinates(double lat, double lng){
+        this.lat = lat;
+        this.lng = lng;
     }
 
-    public double getX() {
-        return xcoord;
+    public double getLat() {
+        return lat;
     }
 
-    public double getY() {
-        return ycoord;
+    public double getYLng() {
+        return lng;
     }
 
 
