83aacbcc7aed4ed80f9f553b555c356315641494
fonfalleh <jacob.rippe@gmail.com>
2015-05-13 11:52:46 +0200

    Restored methods lost in a merge accident

--- app/src/main/java/com/npfom/odin/MainActivity.java
+++ app/src/main/java/com/npfom/odin/MainActivity.java
@@ -3,6 +3,7 @@ package com.npfom.odin;
 
 import android.content.Context;
 import android.content.Intent;
+import android.location.Criteria;
 import android.location.Location;
 import android.location.LocationManager;
 import android.os.Bundle;
@@ -18,12 +19,15 @@ import java.text.DateFormatSymbols;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
 import java.util.Random;
+import com.google.android.gms.maps.model.LatLng;
 
 public class MainActivity extends ActionBarActivity {
 
     EditText editComplaint;
     EditText editName;
     TextView responseText;
+    private LocationManager locationManager;
+    private String provider;
     TextView timeView;
     TextView dateView;
     int time = -1;
@@ -43,6 +47,14 @@ public class MainActivity extends ActionBarActivity {
         editComplaint = (EditText) findViewById(R.id.editComplaint);
         editName = (EditText) findViewById(R.id.editName);
         responseText = (TextView) findViewById(R.id.responseText);
+
+        //TODO testing shared coordinates
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        // Define the criteria how to select the location provider -> use
+        // default
+        Criteria criteria = new Criteria();
+        provider = locationManager.getBestProvider(criteria, false);
+        updateLocation(); //TODO Does not use provider anymore, might be null...
 
         //Get pointers to date and time fields
         timeView = (TextView) findViewById(R.id.showTime);
@@ -108,13 +120,6 @@ public class MainActivity extends ActionBarActivity {
         Intent intent = new Intent(this, ShowLocationActivity.class);
         startActivity(intent);
     }
-
-    public void openMap(View view) {
-        Intent intent = new Intent(this, MapsActivity.class);
-        startActivity(intent);
-    }
-
-
     public void pickDate(View view) {
         Intent intent = new Intent(this, DatePickerActivity.class);
         startActivityForResult(intent, DATE_REQUEST);
@@ -151,6 +156,12 @@ public class MainActivity extends ActionBarActivity {
                     updateDate();
                 }
             }
+        } else if (requestCode == 5){
+            double lat = data.getDoubleExtra("lat", 0);
+            double lng = data.getDoubleExtra("lng", 0);
+            Log.d("Main, getting marker", "lat:" + lat + " lng:" +lng );
+            //TODO make more pretty and useful
+            //TODO Use the coordinates.
         }
     }
 
@@ -198,5 +209,21 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+    public void openMap(View view){
+        updateLocation();
+        Intent intent = new Intent(this, MapsActivity.class);
+        startActivity(intent);
+    }
+    public void openMarkerMap(View view){
+        updateLocation();
+        Intent intent = new Intent(this, MapMarker.class);
+        //startActivity(intent);
+        int requestcode = 5; // A fair diceroll
+        startActivityForResult (intent, requestcode);
+    }
+    private void updateLocation(){
+        Location location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER); //provider
+        LatLngHolder.updateLatLng(location);
     }
 }
